a801203e2cf232e0ad3b14e274591bbabfab13b4
xiaojun89 <xiaojun_89@126.com>
2015-02-16 11:25:53 +0800

    >支持加载“微软雅黑”字体，字体需放在启动程序的根目录下 by zxj

--- AdvaMACSystem/mainForm.cs
+++ AdvaMACSystem/mainForm.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace AdvaMACSystem
 {
@@ -22,5 +24,42 @@ namespace AdvaMACSystem
             lb_Now.Text = string.Format("{0:00}-{1:00}-{2:00} {3:00}:{4:00}:{5:00}", dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
 
         }
+
+#region 加载字体
+#if WindowsCE
+        //新增字体
+        [DllImport("coredll", EntryPoint = "AddFontResource")]
+        private static extern int AddFontResource([In, MarshalAs(UnmanagedType.BStr)]string fontSource);
+
+        [DllImport("coredll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+#endif
+
+        private bool isFontExists()
+        {
+            System.Drawing.Text.InstalledFontCollection fonts = new System.Drawing.Text.InstalledFontCollection();
+            foreach (System.Drawing.FontFamily family in fonts.Families)
+            {
+                if (family.Name == "微软雅黑")
+                    return true;
+            }
+            return false;
+        }
+
+        private void LoadFont()
+        {
+#if WindowsCE
+            if (!isFontExists())
+            {
+                if (File.Exists(@"HardDisk2\MSYH.ttf"))
+                {
+                    int installFont = AddFontResource(@"HardDisk2\MSYH.ttf");
+                    SendMessage((IntPtr)0xffff, 0x001d, IntPtr.Zero, IntPtr.Zero);
+                }
+            }
+#endif
+        }
+#endregion
+
     }
 }
