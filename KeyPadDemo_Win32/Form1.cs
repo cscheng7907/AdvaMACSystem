aa9b5bb227d4bbf665c466c12b73e200ce252bc5
cscheng <cscheng7907@gmail.com>
2015-02-18 14:28:42 +0800

    >WIN32工程的添加

---
+++ KeyPadDemo_Win32/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ComCtrls;
+
+
+namespace KeyPadDemo_Win32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("MouseClick");
+
+
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("textBox1_Click");
+            KeypadForm f = KeypadForm.GetKeypadForm(textBox1.Text);
+            if (f.ShowDialog(this) == DialogResult.OK)
+                textBox1.Text = f.KeyText;
+
+        }
+    }
+}
