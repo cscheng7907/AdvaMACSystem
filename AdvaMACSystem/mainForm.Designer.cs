6f8498d79622c895d101e00eae11be9180928650
cs <cscheng7907@gmail.com>
2015-02-21 14:31:56 +0800

    >故障页面的添加，未实现

--- AdvaMACSystem/mainForm.Designer.cs
+++ AdvaMACSystem/mainForm.Designer.cs
@@ -28,8 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.timer1 = new System.Windows.Forms.Timer();
-            this.lb_Now = new System.Windows.Forms.Label();
+            this.lb_date = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.LabelCurError = new ComCtrls.ImageLabel();
+            this.label_CurWarning = new ComCtrls.ImageLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -37,24 +48,120 @@
             this.timer1.Interval = 500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // lb_Now
+            // lb_date
             // 
-            this.lb_Now.Location = new System.Drawing.Point(726, 38);
-            this.lb_Now.Name = "lb_Now";
-            this.lb_Now.Size = new System.Drawing.Size(204, 20);
-            this.lb_Now.Text = "2015-02-12 12:11:18";
+            this.lb_date.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular);
+            this.lb_date.Location = new System.Drawing.Point(834, 15);
+            this.lb_date.Name = "lb_date";
+            this.lb_date.Size = new System.Drawing.Size(187, 27);
+            this.lb_date.Text = "日期：2015-02-12";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LabelCurError);
+            this.panel1.Controls.Add(this.label_CurWarning);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lb_time);
+            this.panel1.Controls.Add(this.lb_date);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1024, 90);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(268, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.Text = "工程编号：XXXX";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(268, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 20);
+            this.label3.Text = "工程名称：XXXXXXXX";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 90);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular);
+            this.label2.Location = new System.Drawing.Point(112, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(337, 28);
+            this.label2.Text = "HIS0350A钢支撑轴力补偿系统";
+            // 
+            // lb_time
+            // 
+            this.lb_time.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular);
+            this.lb_time.Location = new System.Drawing.Point(834, 54);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(187, 30);
+            this.lb_time.Text = "时间：12 : 11 : 18";
+            // 
+            // LabelCurError
+            // 
+            this.LabelCurError.BackImg = ((System.Drawing.Image)(resources.GetObject("LabelCurError.BackImg")));
+            this.LabelCurError.Checked = false;
+            this.LabelCurError.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular);
+            this.LabelCurError.IMGContainer = null;
+            this.LabelCurError.Layout = ComCtrls.KTLayout.GlyphTop;
+            this.LabelCurError.Location = new System.Drawing.Point(734, 15);
+            this.LabelCurError.Name = "LabelCurError";
+            this.LabelCurError.Size = new System.Drawing.Size(81, 72);
+            this.LabelCurError.TabIndex = 14;
+            this.LabelCurError.Text = "33";
+            this.LabelCurError.TextX = 55F;
+            this.LabelCurError.TextY = 51F;
+            this.LabelCurError.TransParent = false;
+            // 
+            // label_CurWarning
+            // 
+            this.label_CurWarning.BackImg = ((System.Drawing.Image)(resources.GetObject("label_CurWarning.BackImg")));
+            this.label_CurWarning.Checked = false;
+            this.label_CurWarning.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular);
+            this.label_CurWarning.IMGContainer = null;
+            this.label_CurWarning.Layout = ComCtrls.KTLayout.GlyphTop;
+            this.label_CurWarning.Location = new System.Drawing.Point(647, 15);
+            this.label_CurWarning.Name = "label_CurWarning";
+            this.label_CurWarning.Size = new System.Drawing.Size(81, 72);
+            this.label_CurWarning.TabIndex = 7;
+            this.label_CurWarning.Text = "60";
+            this.label_CurWarning.TextX = 55F;
+            this.label_CurWarning.TextY = 51F;
+            this.label_CurWarning.TransParent = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 90);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1024, 56);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1022, 743);
+            this.ClientSize = new System.Drawing.Size(1024, 768);
             this.ControlBox = false;
-            this.Controls.Add(this.lb_Now);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "mainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -62,7 +169,16 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label lb_Now;
+        private System.Windows.Forms.Label lb_date;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lb_time;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private ComCtrls.ImageLabel label_CurWarning;
+        private ComCtrls.ImageLabel LabelCurError;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- AdvaMACSystem/mainForm.cs
+++ AdvaMACSystem/mainForm.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Threading;
+using DataPool;
 
 namespace AdvaMACSystem
 {
@@ -18,13 +20,22 @@ namespace AdvaMACSystem
             InitializeComponent();
         }
 
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            WarnErrOper = new WarnErrOperator();
+
+            WarnErrOper.CanDatapool = CDataPool.GetDataPoolObject();
+
+            WarnErrOper.OnWarnErrChanged += new EventHandler(DoWarn);
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             DateTime dt = DateTime.Now;
-            lb_Now.Text = string.Format("{0:00}-{1:00}-{2:00} {3:00}:{4:00}:{5:00}", dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+            lb_date.Text = string.Format("{0:00}-{1:00}-{2:00} {3:00}:{4:00}:{5:00}", dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
         }
 
-#region 加载字体
+        #region 加载字体
 #if WindowsCE
         //新增字体
         [DllImport("coredll", EntryPoint = "AddFontResource")]
@@ -58,7 +69,52 @@ namespace AdvaMACSystem
             }
 #endif
         }
-#endregion
+        #endregion
+
+        #region 报警和故障的扫描
+        private WarnErrOperator WarnErrOper = null;
+
+
+        private Thread WarnErrThread;
+        private int WarnErrThreadInterval = 500;
+        private bool WarnErrThreadrunning = false;
+        private void Start()
+        {
+            if (WarnErrThreadrunning)
+                return;
+
+            WarnErrThread = new Thread(new ThreadStart(WarnErrThreadMethod));
+            WarnErrThread.Start();
+
+        }
+
+        private void Stop()
+        {
+            WarnErrThreadrunning = false;
+
+        }
+
+        private void WarnErrThreadMethod()
+        {
+            WarnErrThreadrunning = true;
+            while (WarnErrThreadrunning)
+            {
+                Thread.Sleep(WarnErrThreadInterval);
+
+                if (WarnErrOper != null)
+                    WarnErrOper.Update();
+            }
+        }
+
+        private void DoWarn(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        #endregion
+
 
     }
 }
